d0bc3de6e38c7030028bf49d78fef1049c2bd5b4
SidharathBansal <sidharath0794.cse19@chitkara.edu.inl>
2022-06-22 15:18:24 +0530

    fixed recommended posts, search and tags

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,141 @@
+import React, { useState, useEffect } from "react";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
+import FileBase from "react-file-base64";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+import useStyles from "./styles";
+import { createPost, updatePost } from "../../actions/posts";
+
+const Form = ({ currentId, setCurrentId }) => {
+  const [postData, setPostData] = useState({
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
+  const post = useSelector((state) =>
+    currentId ? state.posts.posts.find((p) => p._id === currentId) : null
+  );
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const user = JSON.parse(localStorage.getItem("profile"));
+
+  useEffect(() => {
+    if (post) setPostData(post);
+  }, [post]);
+
+  const clear = () => {
+    setCurrentId(null);
+    setPostData({
+      title: "",
+      message: "",
+      tags: "",
+      selectedFile: "",
+    });
+  };
+
+  const handleTagsChange = (e) => {
+    let tags =  e.target.value.replace(/ /g,'');
+    setPostData({ ...postData, tags: tags.split(",") })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    currentId
+      ? dispatch(
+          updatePost(currentId, { ...postData, name: user?.result?.name })
+        )
+      : dispatch(
+          createPost({ ...postData, name: user?.result?.name }, history)
+        );
+
+    clear();
+  };
+
+  if (!user?.result?.name) {
+    return (
+      <Paper className={classes.paper} elevation={6}>
+        <Typography variant="h6" align="center">
+          Please Sign In to have your own Flashbacks and like other's Flashbacks
+        </Typography>
+      </Paper>
+    );
+  }
+
+  return (
+    <Paper className={classes.paper} elevation={6}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={`${classes.root} ${classes.form}`}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">
+          {currentId ? "Editing" : "Creating"} a Flashback
+        </Typography>
+
+        <TextField
+          name="title"
+          variant="outlined"
+          label="Title"
+          fullWidth
+          value={postData.title}
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+        />
+        <TextField
+          name="message"
+          variant="outlined"
+          label="Message"
+          fullWidth
+          multiline
+          minRows={4}
+          value={postData.message}
+          onChange={(e) =>
+            setPostData({ ...postData, message: e.target.value })
+          }
+        />
+        <TextField
+          name="tags"
+          variant="outlined"
+          label="Tags(comma separated)"
+          fullWidth
+          value={postData.tags}
+          onChange={handleTagsChange}
+        />
+        <div className={classes.fileInput}>
+          <FileBase
+            type="file"
+            multiple={false}
+            onDone={({ base64 }) =>
+              setPostData({ ...postData, selectedFile: base64 })
+            }
+          />
+        </div>
+        <Button
+          className={classes.buttonSubmit}
+          variant="contained"
+          color="primary"
+          size="large"
+          type="submit"
+          fullWidth
+        >
+          Submit
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          size="small"
+          onClick={clear}
+          fullWidth
+        >
+          Clear
+        </Button>
+      </form>
+    </Paper>
+  );
+};
+
+export default Form;
